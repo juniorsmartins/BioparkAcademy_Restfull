a3cef726d727783dc6bca6c3acf16a618c7c5dc6
JuniorMartins <juniorsoaresmartins@gmail.com>
2021-12-09 10:06:41 -0400

    Project Java with Spring Boot + FreeMarker e Bootstrap

---
+++ src/main/java/br/com/bioparkacademy/BioparkacademyApplication.java
@@ -0,0 +1,11 @@
+package br.com.bioparkacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioparkacademyApplication 
+{
+	public static void main(String[] args) 
+        {SpringApplication.run(BioparkacademyApplication.class, args);}
+}
---
+++ src/test/java/br/com/bioparkacademy/BioparkacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bioparkacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioparkacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
